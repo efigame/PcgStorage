4ce5b3d657aaac47e1a4a4c26d9044fe8141eb0c
efigame <kje@efigame.com>
2014-01-14 19:52:54 +0100

    Included SubSkill

---
+++ PcgManager/Dto/SubSkill.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcgManager.Dto
+{
+    public class SubSkill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Adjustment { get; set; }
+        public int BaseSkillId { get; set; }
+
+        public SubSkill()
+        {
+        }
+
+        internal SubSkill(DataAccess.Dto.SubSkill skill) : this(skill, true)
+        {
+        }
+
+        internal SubSkill(DataAccess.Dto.SubSkill skill, bool deepObjects)
+        {
+            Id = skill.Id;
+            Name = skill.Name;
+            Adjustment = skill.Adjustment;
+            BaseSkillId = skill.BaseSkillId;
+        }
+    }
+}
